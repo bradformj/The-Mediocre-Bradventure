5d24738688f5eee82b8ae5c847c6735c4df09697
bradformj <bradformj@users.noreply.github.com>
2017-09-01 09:52:40 -0400

    Modified the world interactions script
    Added placeholders for the different functions of the left and right mouse clicks.  Left to simply focus an interactable.  Right to focus and interact.  Also added a nav mesh agent to the player for some useful functionality like turning to face the interactable.

--- Assets/Scripts/WorldInteraction.cs
+++ Assets/Scripts/WorldInteraction.cs
@@ -5,6 +5,8 @@ using UnityEngine.AI;
 
 public class WorldInteraction : MonoBehaviour {
     UnityEngine.AI.NavMeshAgent playerAgent;
+
+    GameObject focusItem;
     
 	
 	void Start () {
@@ -18,33 +20,62 @@ public class WorldInteraction : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
         {
-            GetInteraction();
-        }
-	}
+            //Check if we hit an interractable
+            //if we did set that item as our focus.
 
-    void GetInteraction()
+            focusItem = GetFocus();
+
+        }
+
+        if (Input.GetMouseButtonDown(1) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()){
+            //Check to see if we hit an interractable
+            //if we did, set that item as our focus and interact
+
+            focusItem = GetFocus();
+            GetInteraction(focusItem);
+        }
+
+    }
+
+    void GetInteraction(GameObject focusObject)
+    {
+        if (focusObject != null)
+        {
+            Debug.Log(focusObject.tag + " " + focusObject.name + " is the target of an interaction.");
+        }
+        else
+        {
+            Debug.Log("Nothing interacted.");
+        }
+    }
+
+    GameObject GetFocus()
     {
         Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit interactionInfo;
+
         if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
         {
             playerAgent.updateRotation = true;
-            GameObject interactedObject = interactionInfo.collider.gameObject;
-            if(interactedObject.tag == "Enemy")
+            GameObject focusObject = interactionInfo.collider.gameObject;
+            if (focusObject.tag == "Enemy")
             {
-                Debug.Log("move to enemy");
-                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+                Debug.Log("Enemy " + focusObject.name + " focused");
+                return focusObject;
             }
-            else if(interactedObject.tag == "Interactable Object")
+            else if (focusObject.tag == "Interactable Object")
             {
-                Debug.Log("move to object");
-                interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
+                Debug.Log("Item " + focusObject.name + " focused");
+                return focusObject;
             }
             else
             {
-                playerAgent.stoppingDistance = 0;
-                playerAgent.destination = interactionInfo.point;
+                Debug.Log("Item focused was neither enemy nor interactabl.");
+                return focusObject;
             }
         }
+        else
+            return null;
+
     }
 }
